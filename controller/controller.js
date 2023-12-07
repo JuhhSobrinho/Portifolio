ad58ac6c123dce96c3a86db66fdbf3bcbf32874c
JuhhSobrinho <juliano.sobrinhojunior@gmail.com>
2023-12-07 17:46:00 -0300

    adicionando os projetos no momento, form do email já funcionando e o resto tbm, falta colocar todos projetos e deixar responsivo

--- controller/controller.js
+++ controller/controller.js
@@ -1,88 +1,147 @@
 import { carregarBancoDeDados } from '../model/model.js';
+import { seTLocarStorage, geTLocalStorage } from "../model/model.js";
 
 const guias = document.getElementById('guias');
 
 const imgFicha = document.getElementById('img-ficha');
 const path = window.location.pathname;
 
-let textGuiaHTML = ''
+let textGuiaHTML = '';
+
+let sectionImgFicha = '';
+
 
 carregarBancoDeDados()
     .then(bd => {
         // Agora você pode acessar os bd do JSO
+
+
+        function saturacion(status) {
+
+            let styleSatu = '';
+            if (status === "Em desenvolvimento") {
+                styleSatu = "filter: saturate(0);"
+            } else {
+                styleSatu = "filter: saturate(1);"
+            }
+
+            return styleSatu;
+        }
+
+
+
+
+        function dados() {
+            sectionImgFicha = `            
+            <section class="sobre-project-img">
+            <a href="${bd[geTLocalStorage("AtualGuia")].linkProj}" class="projeto-container" style="${saturacion(bd[geTLocalStorage("AtualGuia")].status)}">
+            <h1 class="nome-project">${bd[geTLocalStorage("AtualGuia")].nome}</h1>
+                <img  class="img-project" id="img-project" src="${bd[geTLocalStorage("AtualGuia")].img}" alt="foto-do-projeto-${bd[geTLocalStorage("AtualGuia")].nome}">
+                </a>
+                <h1 class="sobre-project-titulo">Sobre o Projeto</h1>
+            <p class="sobre-project-dados">
+                ${bd[geTLocalStorage("AtualGuia")].sobre}
+                </p>
+            </section>
+
+
+            <div class="ficha-card">
+            <h1 class="status">
+            status: ${bd[geTLocalStorage("AtualGuia")].status}
+            </h1>
+            <p class="descricao">
+            ${bd[geTLocalStorage("AtualGuia")].descricao}
+            </p>
+                <span class="ficha-spans" id="responsividade">Responsivo: ${bd[geTLocalStorage("AtualGuia")].responsivo}</span>
+            <a href="${bd[geTLocalStorage("AtualGuia")].linkCod}">
+            <span class="ficha-spans" id="codigoLink">Codigo: ${bd[geTLocalStorage("AtualGuia")].linkCodFake}</span>
+            </a>
+            <a href="${bd[geTLocalStorage("AtualGuia")].linkProj}">
+            <span class="ficha-spans" id="projectLink">Project: ${bd[geTLocalStorage("AtualGuia")].linkProjFake}</span>
+            </a>
+            </div>`;
+            imgFicha.innerHTML = sectionImgFicha;
+        }
+
+        if (path === "/view/projeto.html") {
+            dados();
+        }
+
+
+
+
+
         bd.forEach(element => {
             console.log(element);
 
             textGuiaHTML += `
         <div class="line" id="line-menu"></div>
         <a class="guia" id="${element.id}">
-            <img id="${element.nome}" class="icon-guias" src="${element.icon}" alt="guia-meuperfil" />
+            <img id="${element.nome}" class="icon-guias" src="${element.icon}" style="${saturacion(element.status)}" alt="guia-${element.nome}" />
         </a>`;
 
         });
-
         guias.innerHTML = textGuiaHTML;
+
+
+
+
 
         const links = guias.querySelectorAll('a.guia');
 
         links.forEach(link => {
             link.addEventListener('click', function () {
-                console.log('teste');
-                let sectionImgFicha = '';
+                seTLocarStorage("AtualGuia", link.id);
+                console.log(geTLocalStorage("AtualGuia"));
+
+
                 if (link.id === "0") {
                     window.location.href = './index.html';
-                }else if (path === "/view/projeto.html") {
-                    console.log(path);
-                    dados()
                 } else {
-                    window.location.href = './projeto.html';
-                    dados()
+                    window.location.href = './projeto.html'; // Tempo de espera em milissegundos (pode ajustar conforme necessário)
                 }
-
-function dados() {
-    sectionImgFicha = `            
-    <section class="sobre-project-img">
-    <a href="${bd[link.id].LinkProj}" class="projeto-container">
-        <h1 class="nome-project">${bd[link.id].nome}</h1>
-            <img class="img-project" id="img-project" src="${bd[link.id].img}" alt="foto-do-projeto">
-    </a>
-    <h1 class="sobre-project-titulo">Sobre o Projeto</h1>
-    <p class="sobre-project-dados">
-    ${bd[link.id].sobre}
-    </p>
-</section>
-
-
-<div class="ficha-card">
-    <h1 class="status">
-        status: ${bd[link.id].status}
-    </h1>
-    <p class="descricao">
-    ${bd[link.id].descricao}
-    </p>
-    <span class="ficha-spans" id="responsividade">Responsivo: ${bd[link.id].responsivo}</span>
-    <a href="${bd[link.id].linkCod}">
-        <span class="ficha-spans" id="codigoLink">Codigo: ${bd[link.id].linkCodFake}</span>
-    </a>
-    <a href="${bd[link.id].LinkProj}">
-        <span class="ficha-spans" id="projectLink">Project: ${bd[link.id].linkProjFake}</span>
-    </a>
-</div>`;
-            imgFicha.innerHTML = sectionImgFicha;
-}
-
-
-
-                console.log(`ID do <a> clicado: ${link.id}`);
-
-
             });
         });
+
+
+
 
     })
     .catch(error => {
         // Trata erros que podem ocorrer durante o carregamento
         console.error('Erro ao carregar o banco de bd:', error);
     });
+console.log(geTLocalStorage("AtualGuia"));
+
+
+
+
+// Inicializar o Email.js fora do evento de clique
+emailjs.init("NSP0Hhxyw2c7dRuK3");
+
+const btn = document.getElementById('btnEnviar');
+
+btn.addEventListener('click', () => {
+    var nome = document.getElementById("nome").value;
+    var email = document.getElementById("email").value;
+    var mensagem = document.getElementById("msg").value;
+
+    const templateParams = {
+        to_email: "juliano.sobrinhojunior@gmail.com",
+        subject: nome,
+        mensagem: `Nome: ${nome}\nE-mail: ${email}\n \nMensagem:\n ${mensagem}`,
+    };
+
+    emailjs.send("service_q34eppz", "template_l8pven8", templateParams)
+        .then((response) => {
+            console.log("E-mail enviado com sucesso:", response);
+            alert("E-mail enviado com sucesso!");
+        })
+        .catch((error) => {
+            console.error("Erro ao enviar o e-mail:", error);
+            alert("Erro ao enviar o e-mail. Por favor, tente novamente mais tarde.");
+        });        
+});
+
 
 
--- model/model.js
+++ model/model.js
@@ -6,3 +6,12 @@ export function carregarBancoDeDados() {
             console.error('Erro ao carregar o arquivo JSON:', error);
         });
 }
+
+
+export function seTLocarStorage(chave, value){
+    localStorage.setItem(chave, JSON.stringify(value)); ///O json pega a parada muda pra string e salva nele. ////salva a ids
+}
+
+export function geTLocalStorage(chave){
+    return  JSON.parse(localStorage.getItem(chave)); ///nesse caso é o contrario, json pega e transforma em objeto oq era texto/string
+}
